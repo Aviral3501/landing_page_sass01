d707fdab1dabf03fbbf459a7d553d1197067f6a9
Aviral Singh <aviralsingh3501@gmail.com>
2024-07-30 20:29:02 +0530

    logo section done

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -18,22 +18,45 @@ const images = [
 
 export const LogoTicker = () => {
 
-  return(
+  return (
     <>
-    <div className="bg-black text-white py-[72px]">
-      <div className="container">
-        <h2 className="text-lg text-center sm:text-2xl md:text-xl">Trusted by the world's most innovative teams.</h2>
-        <div className="overflow-hidden gap-16">
-           <div className="flex">
+      <div className="bg-black text-white py-[40px] ">
+        <div className="container">
+          <h2 className="text-xl text-center sm:text-3xl md:text-2xl text-white/70">Trusted by the world's most innovative teams.</h2>
+          {/* slight vanishing blurry effect - fadein and fadeout effect*/}
+           <div className="overflow-hidden mt-9 sm:mt-10 md:mt-11 lg:mt-12 before:content-['']  after:content-[''] before:absolute
+         after:absolute before:h-full after:h-full before:w-10 after:w-10 relative  after:right-0 before:left-0 before:top-0 after:top-0
+         before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))]  after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+           <div className="flex gap-16 sm:gap-x-28 animate-scroll">
            {images.map(({src,alt})=>(
               <Image src={src} alt={alt} className="flex-none h-8 w-auto"/>
             ))}
+             {images.map(({ src, alt }) => (
+              <Image key={`duplicate-${alt}`} src={src} alt={alt} className="flex-none h-8 w-auto" />
+            ))}
            </div>
+        </div>
         </div>
       </div>
 
-
-    </div>
     </>
   )
 };
+
+
+
+
+
+
+        // <div className="overflow-hidden mt-9 sm:mt-10 md:mt-11 lg:mt-12 relative before:content-[''] after:content-[''] before:absolute 
+        // after:absolute before:h-full after:h-full before:w-10 after:w-10  after:right-0 before:left-0 before:top-0 after:top-0
+        //  before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+        //   <div className="flex gap-16 sm:gap-x-28 animate-scroll">
+        //     {images.map(({ src, alt }) => (
+        //       <Image key={alt} src={src} alt={alt} className="flex-none h-8 w-auto" />
+        //     ))}
+        //     {images.map(({ src, alt }) => (
+        //       <Image key={`duplicate-${alt}`} src={src} alt={alt} className="flex-none h-8 w-auto" />
+        //     ))}
+        //   </div>
+        // </div>
